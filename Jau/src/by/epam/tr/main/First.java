49738ca5d6779524a7ed89e88bdd3d908f5ab80a
JauhienK <81434268+JauhienK@users.noreply.github.com>
2021-03-26 19:48:10 +0300

    Add files via upload

---
+++ Jau/src/by/epam/tr/main/First.java
@@ -0,0 +1,10 @@
+package by.epam.tr.main;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!!!");
+	}
+
+}
